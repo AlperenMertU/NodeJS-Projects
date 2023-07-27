8155887d46e7f4b4b4b0be604c3d663eb73d4481
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-07-27 03:19:51 +0300

    discord bot first steps...

--- discordBot/src/bot.js
+++ discordBot/src/bot.js
@@ -1,3 +1,16 @@
- require("dotenv").config()
+const { Client, Intents } = require('discord.js');
+require("dotenv").config();
+const client = new Client({intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });                                         
+const mySecret = process.env['TOKEN'];
+ client.on("ready", () => {
+ console.log(`Logged is as ${client.user.tag}!`);
+});
 
- console.log(process.env.DİCORDJS_BOT_TOKEN);
+
+client.on("message", (msg) => {
+if(msg.content === "ping"){
+msg.reply("pong");
+}
+});
+
+client.login(mySecret);
